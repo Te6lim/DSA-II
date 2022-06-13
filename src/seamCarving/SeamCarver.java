a7e5bfe4d28e64c74bbf69da1bc911d011605f58
Teslim Akande <teslimakande@gmail.com>
2022-06-13 12:28:57 +0100

    implemented corner cases and constructor specifications

--- src/seamCarving/SeamCarver.java
+++ src/seamCarving/SeamCarver.java
@@ -2,13 +2,26 @@ package seamCarving;
 
 import edu.princeton.cs.algs4.Picture;
 
+import java.util.Arrays;
+
 public class SeamCarver {
 
-    private Picture mPicture;
+    private final Picture mPicture;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        validateObject(picture);
+        mPicture = buildPicture(picture);
+    }
 
+    private Picture buildPicture(Picture picture) {
+        Picture p = new Picture(picture.width(), picture.height());
+        for (int x = 0; x < picture.width(); ++x) {
+            for (int y = 0; y < picture.height(); ++y) {
+                p.set(x, y, picture.get(x, y));
+            }
+        }
+        return p;
     }
 
     // current picture
@@ -28,7 +41,8 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-
+        verifyXRange(x);
+        verifyYRange(y);
         return 0f;
     }
 
@@ -44,12 +58,57 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        validateObject(seam);
+        verifyHorizontalSeam(seam);
+        validatePictureDimension(mPicture.width());
+    }
 
+    private void validatePictureDimension(int dimension) {
+        if (dimension <= 1) throw new IllegalArgumentException();
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        validateObject(seam);
+        verifyVerticalSeam(seam);
+        validatePictureDimension(mPicture.height());
+    }
 
+    private void validateObject(Object object) {
+        if (object == null) throw new IllegalArgumentException();
+    }
+
+    private void verifyXRange(int x) {
+        if (x < 0 || x > mPicture.width() - 1) throw new IllegalArgumentException();
+    }
+
+    private void verifyYRange(int y) {
+        if (y < 0 || y > mPicture.height() - 1) throw new IllegalArgumentException();
+    }
+
+    private void verifyVerticalSeam(int[] seam) {
+        if (seam.length < mPicture.height()) throw new IllegalArgumentException();
+        Arrays.sort(seam);
+        verifyYRange(seam[0]);
+        verifyYRange(seam[seam.length - 1]);
+        if (hasBreakage(seam)) throw new IllegalArgumentException();
+    }
+
+    private void verifyHorizontalSeam(int[] seam) {
+        if (seam.length < picture().width()) throw new IllegalArgumentException();
+        Arrays.sort(seam);
+        verifyXRange(seam[0]);
+        verifyXRange(seam[seam.length - 1]);
+        if (hasBreakage(seam)) throw new IllegalArgumentException();
+    }
+
+    private boolean hasBreakage(int[] seam) {
+        int temp = -1;
+        for (int i : seam) {
+            if (i != temp + 1) return true;
+            ++temp;
+        }
+        return false;
     }
 
     // unit testing (optional)
